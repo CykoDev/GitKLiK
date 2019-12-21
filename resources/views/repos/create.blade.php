c2c6105935fc92c6e89e05c9b7726140e9f12470
ubaidasim514 <ubaidasim514@gmail.com>
2019-12-21 18:43:26 +0500

    New repository Model created

--- resources/views/repos/create.blade.php
+++ resources/views/repos/create.blade.php
@@ -9,6 +9,7 @@
    
 	<link href="{{ asset('css/codeCss.css') }}" rel="stylesheet">
     <link href="{{ asset('css/tabsCss.css') }}" rel="stylesheet">
+   
     
     <div class="container-fluid mt--7">
        
@@ -27,12 +28,18 @@
                                 
                                     <form>
                                         <input type="text" placeholder="Repository Title" class="form-control form-control-alternative">
-                                    </form>
+                                        <br>
+                                        <input type="text" placeholder="Description" class="form-control form-control-alternative">
                                     
+                                    <br>
                                     <div class="text-center">
                                         <button type="button" class="btn btn-outline-success mt-4">{{ __('Import Existing Repository') }}</button>
-                                        <button type="button" class="btn btn-success mt-4">{{ __('Create New Repository') }}</button>
+                                        <button data-toggle="modal" data-target="#myModal" type="button" class="btn btn-success mt-4">{{ __('Create New Repository') }}</button>
                                     </div>
+                                    
+                                    
+                                    </form>
+                                    
                     </div>
 
                     <div class="text-center">
@@ -65,8 +72,48 @@
                     
                     </div>
                 </div>
+                <!-- Modal -->
+                  <div  style="margin-top:5%" class="modal fade" id="myModal" role="dialog">
+                    <div style="height:90%" class="modal-dialog">
+                        
+                    <form>
+                      <!-- Modal content-->
+                      <div style="height:100%" class="modal-content">
+                        <div class="modal-header">
+                          <h4 class="modal-title">Create a Repository</h4>
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          
+                        </div>
+                        
+            
+                        
+                        <div class="modal-body">
+                         <hr>
+                          <p>Create Repository Using:</p>
+                          
+                            <div class="custom-control custom-checkbox">
+                              <input type="checkbox" class="custom-control-input" id="defaultChecked1" checked>
+                              <label class="custom-control-label" for="defaultChecked1">.gitignore</label>
+                            </div>
+                            
+                            <div class="custom-control custom-checkbox">
+                              <input type="checkbox" class="custom-control-input" id="defaultChecked2" checked>
+                              <label class="custom-control-label" for="defaultChecked2">README.md</label>
+                            </div>
+                        </div>
+                        
+                        <div class="modal-footer">
+                         <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                         <button type="button" class="btn btn-success" >Create Repository</button>
+                        </div>
+                      </div>
+                    </form>  
+                      
+
+                    </div>
+                  </div>
                 @include('layouts.footers.auth')
-            </div>
+            
         
         
        
