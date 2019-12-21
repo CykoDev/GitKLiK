fd591c8870887a2f08a2edf740645725ddc0a2b4
AIT <anas.tasadduq@gmail.com>
2019-12-21 22:33:59 +0500

    fix routes for the repositories

--- app/Http/Controllers/RepoController.php
+++ app/Http/Controllers/RepoController.php
@@ -34,22 +34,18 @@ class RepoController extends Controller
 
     public function update(UserRequest $request, User  $user)
     {
-
     }
 
     public function file_update($path)
     {
-
     }
 
     public function destroy(User  $user)
     {
-
     }
 
     public function destroy_file($path)
     {
-
     }
 
 
@@ -76,7 +72,7 @@ class RepoController extends Controller
 
         $targetPath = $repoName;
         for ($i = 1; $i < sizeof($pathElements); $i++) {
-            $targetPath = $targetPath.'\\'.$pathElements[$i];
+            $targetPath = $targetPath . '\\' . $pathElements[$i];
         }
 
         $user = User::whereName($userName)->first();
@@ -84,12 +80,12 @@ class RepoController extends Controller
         $stars = $repo->stars;
 
         $starUsers = [];
-        foreach($stars as $star) {
+        foreach ($stars as $star) {
             array_push($starUsers, $star->user);
         }
 
-        $absoluteRepoPath = storage_path().'\app\repos\clones\\'.$targetPath.'\\';
-        $relativeRepoPath = 'repos\clones\\'.$targetPath;
+        $absoluteRepoPath = storage_path() . '\app\repos\clones\\' . $targetPath . '\\';
+        $relativeRepoPath = 'repos\clones\\' . $targetPath;
         $pathSize = strlen($relativeRepoPath);
 
         $filePaths = Storage::files($relativeRepoPath);
@@ -126,7 +122,6 @@ class RepoController extends Controller
 
     public function file_show($path)
     {
-
     }
 
 
@@ -141,7 +136,7 @@ class RepoController extends Controller
 
         $repoName = $request['repoName'];
 
-        switch ($request['submit']){
+        switch ($request['submit']) {
 
             case 'new':
 
@@ -149,16 +144,16 @@ class RepoController extends Controller
                     'user_id' => $user->id,
                     'name' => $repoName,
                     'description' => $request['repoDesc'],
-                    ]);
+                ]);
                 User::findOrFail(Auth::user()->id)->repos()->save($repo);
 
                 if ($request->has('readme')) {
 
-                    Storage::put('repos/clones/'.$repoName.'/README.md', 'This is a readme file.');
+                    Storage::put('repos/clones/' . $repoName . '/README.md', 'This is a readme file.');
                 }
                 if ($request->has('gitignore')) {
 
-                    Storage::put('repos/clones/'.$repoName.'/.gitignore', '');
+                    Storage::put('repos/clones/' . $repoName . '/.gitignore', '');
                 }
 
                 return redirect(route('repo.view', [Auth::user()->name, $repoName]));
@@ -168,7 +163,7 @@ class RepoController extends Controller
 
             case 'import':
 
-                if (!Git::cloneRemote($name)){
+                if (!Git::cloneRemote($name)) {
 
                     return 'ERROR: git clone error';
                 }
@@ -186,15 +181,16 @@ class RepoController extends Controller
     |--------------------------------------------------------------------------
     */
 
-    public function create_import($title){
+    public function create_import($title)
+    {
 
-        $absolutePath = storage_path().'\app\public\repos\remotes\\'.$title.'.git\\';
+        $absolutePath = storage_path() . '\app\public\repos\remotes\\' . $title . '.git\\';
         $data = [
             'title' => $title,
             'absolutePath' => $absolutePath,
         ];
 
-        if (!Git::initBare($title)){
+        if (!Git::initBare($title)) {
             return 'ERROR: git init bare error';
         }
 
@@ -203,13 +199,12 @@ class RepoController extends Controller
 
     public function create()
     {
-
         return view('repos.create');
     }
 
     public function create_directory(Request $request)
     {
-        $dir = $request['relPath'].'\\'.$request['dirName'];
+        $dir = $request['relPath'] . '\\' . $request['dirName'];
         Storage::makeDirectory($dir);
         return back();
     }
@@ -222,24 +217,26 @@ class RepoController extends Controller
     |--------------------------------------------------------------------------
     */
 
-    public function commit_index(){
+    public function commit_index()
+    {
 
         return view('repo.commits.index');
     }
 
-    public function commit_show(){
+    public function commit_show()
+    {
 
         return view('repo.commits.show');
     }
 
-    public function commit_create(){
+    public function commit_create()
+    {
 
         return view('repo.commits.create');
     }
 
-    public function commit_store(){
-
-
+    public function commit_store()
+    {
     }
 
 
@@ -249,7 +246,8 @@ class RepoController extends Controller
     |--------------------------------------------------------------------------
     */
 
-    public function stars($repoName){
+    public function stars($repoName)
+    {
 
         $repo = Repository::whereName($repoName)->firstOrFail();
 
