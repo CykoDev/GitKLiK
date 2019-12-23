d867395d000a2ed412364b59cf4ee75a0b1391ac
ubaidasim514 <ubaidasim514@gmail.com>
2019-12-24 00:39:11 +0500

    repo index and repo user index UI

--- resources/views/repos/index_user.blade.php
+++ resources/views/repos/index_user.blade.php
@@ -1,20 +1,33 @@
-@extends('layouts.app')
+@extends('layouts.app',['title' => __('User Profile')])
 
 @section('content')
+
+@include('users.partials.header', [
+        'title' => __('My Repositories'),
+        'description' => __('A collection of my repositories'),
+        'class' => 'col-lg-7'
+    ])
+
 
 @php
 use App\User;
 @endphp
 
+<link href="{{ asset('css/tablerows.css') }}" rel="stylesheet">
+<div class="container-fluid mt--7">
+<div class="row">
+<div class="cardContainer col-xl-12 order-xl-1">
+<div class=" card bg-secondary shadow">
+
 <link href="{{ asset('css/dashboardCss.css') }}" rel="stylesheet">
 
-<div class="header orangeGradient pb-8 pt-5 pt-md-8">
+<div class="header  pb-5 pt-5 pt-md-5">
     <div class="container-fluid">
         <div class="header-body">
             <h1>Explore Repositories</h1>
-            <ul>
+            <ul style="margin-top:2%" class="list-group">
                 @foreach($repos as $repo)
-                <li><a href="/{{ User::find($repo->user_id)->name }}/repository/{{ $repo->name }}">{{ $repo->name }}</a>
+                <li style="background:#494948" class="list-group-item"><a class="text-white" href="/{{ User::find($repo->user_id)->name }}/repository/{{ $repo->name }}">{{ $repo->name }}</a>
                 </li>
                 @endforeach
             </ul>
