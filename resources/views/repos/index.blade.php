1199dca16e474508399f2031becd751c71ff044d
AIT <anas.tasadduq@gmail.com>
2019-12-23 23:16:36 +0500

    make repository links on repository index page functional

--- resources/views/repos/index.blade.php
+++ resources/views/repos/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
+@php
+use App\User;
+@endphp
+
 <link href="{{ asset('css/dashboardCss.css') }}" rel="stylesheet">
 
 <div class="header orangeGradient pb-8 pt-5 pt-md-8">
@@ -10,7 +14,8 @@
             <h1>Explore Repositories</h1>
             <ul>
                 @foreach($repos as $repo)
-                <li>{{ $repo->name }}</li>
+                <li><a href="/{{ User::find($repo->user_id)->name }}/repository/{{ $repo->name }}">{{ $repo->name }}</a>
+                </li>
                 @endforeach
             </ul>
         </div>
